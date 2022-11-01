ef23edc85f3bfaee6ae7aaf3c9fdeec3333b7509
moinsoft <mmoin.official@gmail.com>
2022-11-01 23:54:10 +0600

    Problem 002 has been resolved.

---
+++ C Programming/002 Practice Day/problem_002.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    /*
+        Write a C program to declare two integer variables and set values 10 and 20 respectively. Now print the second
+        number and then the first number.
+    */
+
+    int num1 = 10;
+    int num2 = 20;
+
+    printf("%d %d",num2,num1);
+
+
+    getch();
+
+}
+
+

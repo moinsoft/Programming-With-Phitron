f66d77ccb12b5c530fee16ce56f5ecf841662864
moinsoft <mmoin.official@gmail.com>
2022-11-01 19:18:37 +0600

    C input output function

---
+++ C Programming/001 Class Practice/002 C input output function/inputOutput.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2;
+
+    printf("Enter two numbers : ");
+    scanf("%d %d",&num1,&num2);
+
+    printf("\nThe value of num1 is = %d\n",num1);
+    printf("The value of num2 is = %d",num2);
+
+
+    getch();
+}

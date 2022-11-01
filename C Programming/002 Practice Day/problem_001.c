318aa70a39528ee0c114f039cf814847ea4a70cb
moinsoft <mmoin.official@gmail.com>
2022-11-01 23:44:26 +0600

    Problem 001 has been resolved.

---
+++ C Programming/002 Practice Day/problem_001.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    //Write a C program to show output like below:
+
+    printf("Hi there /\\.\tI hope you are doing great and practicing well.\nNow this is a simple task you need to do.\tJust care for the indentation.\nShow 100%% of sincerity.");
+
+
+    getch();
+
+}
+
+

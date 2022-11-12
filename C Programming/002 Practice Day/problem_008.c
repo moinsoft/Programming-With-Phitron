5d6fa158ae20822fb2821f6080c2026d80105b84
moinsoft <mmoin.official@gmail.com>
2022-11-13 01:03:14 +0600

    Problem 008 has been resolved.

---
+++ C Programming/002 Practice Day/problem_008.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+
+    printf("Please enter three integers : ");
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a>b && a>c)
+    {
+        printf("A is bigger");
+    }
+    else if(b>a && b>c)
+    {
+        printf("B is bigger");
+
+    }
+    else if(c>a && c>b)
+    {
+        printf("C is bigger");
+
+    }
+    else
+    {
+        printf("They are equal");
+    }
+
+
+    getch();
+}
+

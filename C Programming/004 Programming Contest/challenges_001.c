1bab139b37bcc859c3d17a1d5360f33392859d4d
moinsoft <mmoin.official@gmail.com>
2022-11-14 02:15:44 +0600

    Challenge 001 has been resolved.

---
+++ C Programming/004 Programming Contest/challenges_001.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main() {
+
+    /* Take two non-negative integer X and Y as input and output the multiplication of those two numbers. */
+
+    int a,b,mul;
+
+    scanf("%d %d",&a,&b);
+
+    mul = a*b;
+
+    printf("%d",mul);
+
+    return 0;
+}
+

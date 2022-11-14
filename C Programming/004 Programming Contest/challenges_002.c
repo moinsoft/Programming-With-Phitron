aacb6331bfeb92ca85af7349239b288ff13d4b05
moinsoft <mmoin.official@gmail.com>
2022-11-14 07:41:12 +0600

    Challenge 002 has been resolved.

---
+++ C Programming/004 Programming Contest/challenges_002.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main() {
+
+    /* Welcome to the Phitron trial programming contest. This is just a simple problem to welcome you. You just need to say Hello! to pass this problem. As simple as that. */
+
+    printf("Hello!");
+
+    return 0;
+}

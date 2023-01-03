2b1edf608e578a904aa37ed7bdd6ea744b596557
moinsoft <mmoin.official@gmail.com>
2023-01-03 19:07:55 +0600

    Printing first 10 natural numbers

---
+++ C Programming/002 Practice Day/problem_017.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    /*
+        Write a program in C to display the first 10 natural numbers.
+
+    */
+
+
+    int i,num=10;
+
+    for(i=1; i<=num; i++)
+    {
+        printf("%d ",i);
+    }
+
+
+    getch();
+}
